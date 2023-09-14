50755c072a018a24702da1964c3c36ea0a6b2fda
EnzoFilangi <enzo.filangi@efrei.net>
2023-09-14 19:20:53 +0200

    Feat : Initialized project

---
+++ src/test/java/org/carbon/MainTest.java
@@ -0,0 +1,12 @@
+package org.carbon;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class MainTest {
+    @Test
+    public void dummyTest(){
+        assertTrue(true);
+    }
+}
