9da9d7e317b5cc09f113e52714845c5b20f52525
EnzoFilangi <enzo.filangi@efrei.net>
2023-09-17 16:53:12 +0200

    Clean : Made the Simulation class easier to read

--- src/main/java/org/carbon/simulation/Simulation.java
+++ src/main/java/org/carbon/simulation/Simulation.java
@@ -20,49 +20,6 @@ public class Simulation {
         this.adventurers = adventurers;
     }
 
-    public void runSimulation(){
-        boolean actionsLeft = true;
-        while (actionsLeft) {
-            displaySimulationState(); // Display the state before updating it to shown the initial map
-            actionsLeft = simulateRound();
-        }
-        // No need to display after the simulation has ended since the state didn't change in the last loop (otherwise actionsLeft wouldn't be false)
-    }
-
-    private boolean simulateRound() {
-        List<PrimedActionCommand> roundActions = buildRoundActionBacklog();
-        if (roundActions.size() == 0){
-            return false;
-        }
-
-        runActionBacklog(roundActions);
-        return true;
-    }
-
-    private List<PrimedActionCommand> buildRoundActionBacklog() {
-        LinkedList<PrimedActionCommand> globalActionBacklog = new LinkedList<>();
-
-        // Build the list of actions for a round by interleaving the actions of the adventurers
-        for (Adventurer adventurer : adventurers){
-            Action action = adventurer.getAndRemoveNextAction();
-            if (action != null){
-                globalActionBacklog.add(new PrimedActionCommand(action, adventurer, map));
-            }
-        }
-
-        return globalActionBacklog;
-    }
-
-    private void runActionBacklog(List<PrimedActionCommand> globalActionBacklog) {
-        for (PrimedActionCommand action: globalActionBacklog) {
-            action.execute();
-        }
-    }
-
-    private void displaySimulationState(){
-        simulationDisplay.display(map, adventurers);
-    }
-
     public RegionMap getMap() {
         return map;
     }
@@ -71,11 +28,67 @@ public class Simulation {
         return adventurers;
     }
 
+    public void runSimulation() {
+        boolean actionsLeft = true;
+        while (actionsLeft) {
+            displaySimulationState(); // Display the state before updating it to show the initial map
+            actionsLeft = simulateRound();
+        }
+        // No need to display after the simulation has ended since the state didn't change in the last loop (otherwise actionsLeft wouldn't be false)
+    }
+
+    /**
+     * Iterate through all adventurers to execute their next action.
+     *
+     * @return true if at least one adventurer did something, false otherwise.
+     */
+    private boolean simulateRound() {
+        List<PrimedActionCommand> roundActions = buildRoundActionBacklog();
+        if (roundActions.size() == 0) {
+            return false;
+        }
+
+        runActionBacklog(roundActions);
+        return true;
+    }
+
+    /**
+     * Extract the next Action from each adventurer and prepares it for execution.
+     *
+     * @return The list of actions, or an empty list if no adventurer has any action left.
+     */
+    private List<PrimedActionCommand> buildRoundActionBacklog() {
+        LinkedList<PrimedActionCommand> globalActionBacklog = new LinkedList<>();
+
+        // Build the list of actions for a round by interleaving the actions of the adventurers
+        for (Adventurer adventurer : adventurers) {
+            Action action = adventurer.getAndRemoveNextAction();
+            if (action != null) {
+                globalActionBacklog.add(new PrimedActionCommand(action, adventurer, map));
+            }
+        }
+
+        return globalActionBacklog;
+    }
+
+    /**
+     * Executes all given actions.
+     */
+    private void runActionBacklog(List<PrimedActionCommand> globalActionBacklog) {
+        for (PrimedActionCommand action : globalActionBacklog) {
+            action.execute();
+        }
+    }
+
+    private void displaySimulationState() {
+        simulationDisplay.display(map, adventurers);
+    }
+
     public String serialize() {
         StringBuilder stringBuilder = new StringBuilder();
 
         stringBuilder.append(map.serialize());
-        for (Adventurer adventurer: adventurers){
+        for (Adventurer adventurer : adventurers) {
             stringBuilder.append(adventurer.serialize());
         }
 
