078b37362789c0c38f0a6a181640725b6d757425
EnzoFilangi <enzo.filangi@efrei.net>
2023-09-17 15:53:22 +0200

    Feat : Implemented simulation lifecycle (create, run, save) and added example map files

--- src/main/java/org/carbon/Main.java
+++ src/main/java/org/carbon/Main.java
@@ -1,7 +1,37 @@
 package org.carbon;
 
+import org.carbon.io.input.SimulationInput;
+import org.carbon.io.input.SimulationInputConfig;
+import org.carbon.io.output.SimulationOutput;
+import org.carbon.io.output.SimulationOutputConfig;
+import org.carbon.simulation.Simulation;
+import org.carbon.simulation.SimulationCreationException;
+import org.carbon.simulation.SimulationFactory;
+
+import java.util.List;
+
 public class Main {
+    private static SimulationInput simulationInput;
+    private static SimulationOutput simulationOutput;
+
+    private static void configure(){
+        simulationInput = SimulationInputConfig.instantiateSimulationInput();
+        simulationOutput = SimulationOutputConfig.instantiateSimulationOutput();
+    }
+
     public static void main(String[] args){
-        System.out.println("Hello, World !");
+        configure();
+
+        try {
+            List<String> initializationCommands = simulationInput.getInitializationCommands();
+            Simulation simulation = SimulationFactory.createSimulationFromRawData(initializationCommands);
+
+            simulation.runSimulation();
+
+            String simulationResultSerialized = simulation.serialize();
+            simulationOutput.saveSimulationResult(simulationResultSerialized);
+        } catch (SimulationCreationException e) {
+            simulationOutput.alertSimulationError(e.getMessage());
+        }
     }
 }
